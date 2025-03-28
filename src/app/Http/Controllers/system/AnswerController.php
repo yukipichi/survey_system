7c5d3d88e973475e954adde24d161e2ee0d826fa
yukipichi <hsgwyk.08@gmail.com>
2025-03-28 11:00:35 +0900

    ファイルの修正をしました

--- src/app/Http/Controllers/system/AnswerController.php
+++ src/app/Http/Controllers/system/AnswerController.php
@@ -22,18 +22,17 @@ class AnswerController extends Controller
          ->paginate(10)
          ->appends(request()->query());
 
-    //各ラベルを追加
+    // 各ラベルを追加
     foreach($answers as $answer){
         $answer->gender_label = Answer::getGenderLabel($answer->gender);
         $answer->age_label = Answer::getAgeLabel($answer->age_id);
         $answer->isSendEmail_label = Answer::getIsSendEmailLabel($answer->is_send_email);
         $answer->feedback_limit = Str::limit($answer->feedback, 30, '...');
     }
-
         return view('system.answer.index', compact('answers'));
     }
 
-    //詳細表示
+    // 詳細表示
     public function show($id)
     {
         $answer = Answer::findOrFail($id);
@@ -45,7 +44,7 @@ class AnswerController extends Controller
         return view('system.answer.details', compact('answer','genderLabel','ageLabel','isSendEmailLabel'));
     }
 
-    //削除処理
+    // 削除処理
     public function destroy($id)
     {
         $answer = Answer::find($id);
@@ -54,7 +53,7 @@ class AnswerController extends Controller
         return redirect()->route('system.answer.index')->with('success', '削除しました');
     }
 
-    //選択削除処理
+    // 選択削除処理
     public function deleteMultiple(Request $request)
     {
         $ids = $request->input('answer', []);
