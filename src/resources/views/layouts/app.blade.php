eb7c34c2b58a5b5f8749582bd36d72adcf8b9df1
yukipichi <hsgwyk.08@gmail.com>
2025-05-21 14:05:40 +0900

    コードの修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -13,8 +13,6 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 	<link href="https://unpkg.com/bootstrap-table@1.24.1/dist/bootstrap-table.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.2/dist/bootstrap-table.min.css">
-
 </head>
 
 <body>
